ab6a82fe28377d53a8374ccf4acd7060340f8146
KiranPawar3930 <95515003+KiranPawar3930@users.noreply.github.com>
2022-01-14 16:41:08 +0530

    up

--- mail.php
+++ mail.php
@@ -1,12 +1,12 @@
 <?php
 $name = $_POST['name'];
 $email = $_POST['email'];
-$subject = $_POST['subject'];
+$project = $_POST['project'];
 $message = $_POST['message'];
 
 $to = "karan3930@gmail.com";
 $sub = "mail from website";
-$txt ="Name = ". $name . "\r\n Email = " . $email . "Subject = ". $subject "\r\nMessage =" . $message;
+$txt ="Name = ". $name . "\r\n Email = " . $email . "project = ". $project "\r\nMessage =" . $message;
 
 $headers ="From: karan3930@gmail.com";
 if($email!=NULL){
